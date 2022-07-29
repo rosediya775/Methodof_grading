927c7ffe1a2f3331d2d5ad07257900abe7f2346a
diyarose775 <diyarose1988@gmail.com>
2022-07-29 04:30:51 -0400

    Second commit

---
+++ Method of Grading/src/gPackage/IfElseIfStatement.java
@@ -0,0 +1,73 @@
+package gPackage;
+
+public class IfElseIfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int marks = 90;
+if (marks <40) {
+	
+	System.out.println("fail");
+}
+
+else if (marks >=40 && marks <50 ) {
+	
+	System.out.println("D grade");
+	
+}
+		
+else if (marks >=50 && marks <60 ) {
+	
+	System.out.println("C grade");	
+	}	
+		
+else if (marks >=60 && marks <70 ) {
+	
+	System.out.println("B grade");}		
+		
+		
+else if (marks >=70 && marks <80 ) {
+	
+	System.out.println("A- grade");	}
+		
+		
+else if (marks >=80 && marks <90 ) {
+	
+	System.out.println("A grade");}
+
+else if (marks >=90 && marks <=100 ) {
+	
+	System.out.println("A+ grade");
+	}
+	
+else {System.out.println("Invalid number");}
+
+	
+	
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+		
+		
+		
+	}
+
+}
